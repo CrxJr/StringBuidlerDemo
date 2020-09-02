6906d5c8a385e284faa4c42fa101c67d6cd67860
CrxJr <50372323+CrxJr@users.noreply.github.com>
2020-09-02 15:03:45 -0400

    Add project files.

---
+++ StringBuidlerDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringBuidlerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder("Hello");
+            sb.AppendLine("This is the 2nd line");
+            sb.Append(".");
+
+            Console.WriteLine(sb.ToString());
+
+        }
+    }
+}
